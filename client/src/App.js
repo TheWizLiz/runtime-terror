be39d4c2018724d5f4b979faeb9e8e33be51bea7
Kingston <liamyoung@ufl.edu>
2021-02-09 16:07:04 -0500

    Another circleCI test. Not working. #3

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,7 @@ import './App.css'
 function App() {
   return (
     <div className="App">
-      <body className="app-body">
-        <p>Learn React</p>
-      </body>
+      <p>Learn React</p>
     </div>
   )
 }
