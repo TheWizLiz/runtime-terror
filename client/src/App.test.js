541121e962ec6a0c3b54cd90fabba7998343af96
Kingston <liamyoung@ufl.edu>
2021-03-12 16:18:44 -0500

    Added one dummy test to make sure test works

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,20 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-/*test('renders learn react link', () => {
+test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Home/i);
   expect(linkElement).toBeInTheDocument();
 });
-
-test('xx', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/xx/i);
-  expect(linkElement).toBeInTheDocument();
-});
-
-test('yy', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/yy/i);
-  expect(linkElement).toBeInTheDocument();
-}); */
