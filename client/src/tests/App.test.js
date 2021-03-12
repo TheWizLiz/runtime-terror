007ee11c38b6b0e1dca1c09252e81ea47c1732b5
Kingston <liamyoung@ufl.edu>
2021-03-12 17:29:12 -0500

    Fixed some tests... Experimented but now ready to merge.

--- client/src/tests/App.test.js
+++ client/src/tests/App.test.js
@@ -1,8 +1,16 @@
 import { render, screen } from '@testing-library/react';
+import TestRenderer from 'react-test-renderer'
 import App from '../App.js';
+import Login from '../components/Login/Login.js'
 
-test('renders learn react link', () => {
+// NOT A VERY GOOD TEST...
+test('Checking for Login Screen Rendering...', () => {
+  const component = TestRenderer.create(<Login />).toJSON()
+  expect(component).toMatchSnapshot()
+})
+
+test('Renders Home Text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Sign In/i);
+  const linkElement = screen.getByText(/Home/i);
   expect(linkElement).toBeInTheDocument();
 });
