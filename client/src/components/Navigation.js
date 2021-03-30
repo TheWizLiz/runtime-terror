b47fe1b88776cf329dcae6185c8a58a19c1f22ca
TheWizLiz <gabriel.turmail@gmail.com>
2021-03-30 16:44:14 -0400

    Some good stuff

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,102 +1,404 @@
 import React from "react";
 import { Link, withRouter } from "react-router-dom";
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import { getFromStorage } from './utils/storage.js'
 
-function Navigation(props) {
-  return (
-    <div className="navigation">
-      <nav class="navbar navbar-expand navbar-dark bg-dark">
-        <div class="container">
-          <Link class="navbar-brand" to="/">
-            Gator Humans versus Zombies
-          </Link>
+class Navigation extends React.Component {
 
-          <div>
-            <ul class="navbar-nav ml-auto">
-              <li
-                class={`nav-item  ${
-                  props.location.pathname === "/" ? "active" : ""
-                }`}
-              >
-                <Link class="nav-link" to="/">
-                  Home
-                  <span class="sr-only">(current)</span>
-                </Link>
-              </li>
-              <li
-                class={`nav-item  ${
-                  props.location.pathname === "/about" ? "active" : ""
-                }`}
-              >
-                <Link class="nav-link" to="/about">
-                  About
-                </Link>
-              </li>
+  constructor (props) {
+    super(props)
+    this.state = {isLoggedIn: false, isAdmin: false}
+    this.validateAdmin = this.validateAdmin.bind(this)
+    this.loc = props.location.pathname;
+  }
 
-              <li
-                class={`nav-item  ${
-                  props.location.pathname === "/Games" ? "active" : ""
-                }`}
-              >
-                <Link class="nav-link" to="/games">
-                  Games
-                </Link>
-              </li>
+  componentDidMount () {
+    const storage = getFromStorage('the_main_app')
+    // console.log(token)
+    if (storage && storage.token) {
+      const { token } = storage
+      fetch('http://localhost:5000/api/account/getAcct/?acct=' + token)
+        .then(res => res.json())
+        .then(player => this.validateAdmin(player))
+        .catch((err) => console.log('An error Occured Loading the Player Data', err))
+    }
+  }
 
-              <li
-                class={`nav-item  ${
-                  props.location.pathname === "/Rules" ? "active" : ""
-                }`}
-              >
-                <Link class="nav-link" to="/rules">
-                  Rules
-                </Link>
-              </li>
+  validateAdmin (player) {
+    if (player.acctType === 'admin') {
+      this.setState({
+        isAdmin: true,
+        isLoggedIn: true
+      })
+    } else if (player.acctType === null) {
+      this.setState({
+        isAdmin: false,
+        isLoggedIn: false
+      })
+    } else {
+      this.setState({
+        isAdmin: false,
+        isLoggedIn: true
+      })
+    }
+  }
 
-              <li
-                class={`nav-item  ${
-                  props.location.pathname === "/Updates" ? "active" : ""
-                }`}
-              >
-                <Link class="nav-link" to="/updates">
-                  Updates
-                </Link>
-              </li>
+  render () {
+    if (this.state.isAdmin) {
+      return (
+        <div className="navigation">
+          <nav class="navbar fixed-top navbar-expand navbar-dark bg-dark">
+            <div class="container">
+              <Link class="navbar-brand" to="/">
+                Gator Humans versus Zombies
+              </Link>
+    
+              <div>
+                <ul class="navbar-nav ml-auto stick">
+                  <li
+                    class={`nav-item  ${
+                      this.loc === "/" ? "active" : ""
+                    }`}
+                  >
+                    <Link class="nav-link" to="/">
+                      Home
+                      <span class="sr-only">(current)</span>
+                    </Link>
+                  </li>
+                  <li
+                    class={`nav-item  ${
+                      this.loc === "/about" ? "active" : ""
+                    }`}
+                  >
+                    <Link class="nav-link" to="/about">
+                      About
+                    </Link>
+                  </li>
+    
+                  <li
+                    class={`nav-item  ${
+                      this.loc === "/Games" ? "active" : ""
+                    }`}
+                  >
+                    <Link class="nav-link" to="/games">
+                      Games
+                    </Link>
+                  </li>
+    
+                  
+                  <NavDropdown title="Rules" id="basic-nav-dropdown">
+                  <li class={`nav-item  ${this.loc === "/PvPRules" ? "active" : ""}`}>
+                    <Link class="dropdown-item" to="/pvp-rules">PvP Rules</Link>
+                    </li>
+                    <li class={`nav-item  ${this.loc === "/CashHuntRules" ? "active" : ""}`}>
+                    <Link class="dropdown-item" to="/cash-hunt-rules">Cash Hunt Rules</Link>
+                    </li>
+                    <li class={`nav-item  ${this.loc === "/ModGuide" ? "active" : ""}`}>
+                    <Link class="dropdown-item" to="/mod-guide">Mod Guide</Link>
+                    </li>
+                    <NavDropdown.Divider />
+                    <li class={`nav-item  ${this.loc === "/PlayerSafetyPlan" ? "active" : ""}`}>
+                    <Link class="dropdown-item" to="/player-safety-plan">Player Safety Plan</Link>
+                    </li>
+                  </NavDropdown>
+    
+                  <li
+                    class={`nav-item  ${
+                      this.loc === "/Updates" ? "active" : ""
+                    }`}
+                  >
+                    <Link class="nav-link" to="/updates">
+                      Updates
+                    </Link>
+                  </li>
+    
+                  <li
+                    class={`nav-item  ${
+                      this.loc === "/Leaderboard" ? "active" : ""
+                    }`}
+                  >
+                    <Link class="nav-link" to="/leaderboard">
+                      Leaderboard
+                    </Link>
+                  </li>
+    
+                  <li
+                    class={`nav-item  ${
+                      this.loc === "/contact" ? "active" : ""
+                    }`}
+                  >
+                    <Link class="nav-link" to="/contact">
+                      Contact
+                    </Link>
+                  </li>
 
-              <li
-                class={`nav-item  ${
-                  props.location.pathname === "/Leaderboard" ? "active" : ""
-                }`}
-              >
-                <Link class="nav-link" to="/leaderboard">
-                  Leaderboard
-                </Link>
-              </li>
+                  <li
+                    class={`nav-item  ${
+                      this.loc === "/AdminDashboard" ? "active" : ""
+                    }`}
+                  >
+                    <Link class="nav-link" to="/admin-dashboard">
+                      Admin Dashboard
+                    </Link>
+                  </li>
 
-              <li
-                class={`nav-item  ${
-                  props.location.pathname === "/contact" ? "active" : ""
-                }`}
-              >
-                <Link class="nav-link" to="/contact">
-                  Contact
-                </Link>
-              </li>
-
-              <li
-                class={`nav-item  ${
-                  props.location.pathname === "/LogIn" ? "active" : ""
-                }`}
-              >
-                <Link class="nav-link" to="/login">
-                  Log In
-                </Link>
-              </li>
-            </ul>
-          </div>
+                  <li
+                    class={`nav-item  ${
+                      this.loc === "/GameCreation" ? "active" : ""
+                    }`}
+                  >
+                    <Link class="nav-link" to="/game-creation">
+                      Game Creation
+                    </Link>
+                  </li>
+    
+                  <li
+                    class={`nav-item  ${
+                      this.loc === "/LogIn" ? "active" : ""
+                    }`}
+                  >
+                    <Link class="nav-link" to="/login">
+                      Log Out
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
         </div>
-      </nav>
-    </div>
-  );
+      );
+    }
+    else if (this.state.isLoggedIn) {
+      return (
+        <div className="navigation">
+          <nav class="navbar fixed-top navbar-expand navbar-dark bg-dark">
+            <div class="container">
+              <Link class="navbar-brand" to="/">
+                Gator Humans versus Zombies
+              </Link>
+    
+              <div>
+                <ul class="navbar-nav ml-auto stick">
+                  <li
+                    class={`nav-item  ${
+                      this.loc === "/" ? "active" : ""
+                    }`}
+                  >
+                    <Link class="nav-link" to="/">
+                      Home
+                      <span class="sr-only">(current)</span>
+                    </Link>
+                  </li>
+                  <li
+                    class={`nav-item  ${
+                      this.loc === "/about" ? "active" : ""
+                    }`}
+                  >
+                    <Link class="nav-link" to="/about">
+                      About
+                    </Link>
+                  </li>
+    
+                  <li
+                    class={`nav-item  ${
+                      this.loc === "/Games" ? "active" : ""
+                    }`}
+                  >
+                    <Link class="nav-link" to="/games">
+                      Games
+                    </Link>
+                  </li>
+    
+                  
+                  <NavDropdown title="Rules" id="basic-nav-dropdown">
+                  <li class={`nav-item  ${this.loc === "/PvPRules" ? "active" : ""}`}>
+                    <Link class="dropdown-item" to="/pvp-rules">PvP Rules</Link>
+                    </li>
+                    <li class={`nav-item  ${this.loc === "/CashHuntRules" ? "active" : ""}`}>
+                    <Link class="dropdown-item" to="/cash-hunt-rules">Cash Hunt Rules</Link>
+                    </li>
+                    <li class={`nav-item  ${this.loc === "/ModGuide" ? "active" : ""}`}>
+                    <Link class="dropdown-item" to="/mod-guide">Mod Guide</Link>
+                    </li>
+                    <NavDropdown.Divider />
+                    <li class={`nav-item  ${this.loc === "/PlayerSafetyPlan" ? "active" : ""}`}>
+                    <Link class="dropdown-item" to="/player-safety-plan">Player Safety Plan</Link>
+                    </li>
+                  </NavDropdown>
+    
+                  <li
+                    class={`nav-item  ${
+                      this.loc === "/Updates" ? "active" : ""
+                    }`}
+                  >
+                    <Link class="nav-link" to="/updates">
+                      Updates
+                    </Link>
+                  </li>
+
+                  <li
+                    class={`nav-item  ${
+                      this.loc === "/ActionLog" ? "active" : ""
+                    }`}
+                  >
+                    <Link class="nav-link" to="/action-log">
+                      Log Kill
+                    </Link>
+                  </li>
+                  
+                  <li
+                    class={`nav-item  ${
+                      this.loc === "/AccountDetails" ? "active" : ""
+                    }`}
+                  >
+                    <Link class="nav-link" to="/account-details">
+                      Account Details
+                    </Link>
+                  </li>
+    
+                  <li
+                    class={`nav-item  ${
+                      this.loc === "/Leaderboard" ? "active" : ""
+                    }`}
+                  >
+                    <Link class="nav-link" to="/leaderboard">
+                      Leaderboard
+                    </Link>
+                  </li>
+    
+                  <li
+                    class={`nav-item  ${
+                      this.loc === "/contact" ? "active" : ""
+                    }`}
+                  >
+                    <Link class="nav-link" to="/contact">
+                      Contact
+                    </Link>
+                  </li>
+    
+                  <li
+                    class={`nav-item  ${
+                      this.loc === "/LogIn" ? "active" : ""
+                    }`}
+                  >
+                    <Link class="nav-link" to="/login">
+                      Log Out
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+        </div>
+      );
+    }
+    else {
+      return (
+        <div className="navigation">
+          <nav class="navbar fixed-top navbar-expand navbar-dark bg-dark">
+            <div class="container">
+              <Link class="navbar-brand" to="/">
+                Gator Humans versus Zombies
+              </Link>
+    
+              <div>
+                <ul class="navbar-nav ml-auto stick">
+                  <li
+                    class={`nav-item  ${
+                      this.loc === "/" ? "active" : ""
+                    }`}
+                  >
+                    <Link class="nav-link" to="/">
+                      Home
+                      <span class="sr-only">(current)</span>
+                    </Link>
+                  </li>
+                  <li
+                    class={`nav-item  ${
+                      this.loc === "/about" ? "active" : ""
+                    }`}
+                  >
+                    <Link class="nav-link" to="/about">
+                      About
+                    </Link>
+                  </li>
+    
+                  <li
+                    class={`nav-item  ${
+                      this.loc === "/Games" ? "active" : ""
+                    }`}
+                  >
+                    <Link class="nav-link" to="/games">
+                      Games
+                    </Link>
+                  </li>
+    
+                  
+                  <NavDropdown title="Rules" id="basic-nav-dropdown">
+                  <li class={`nav-item  ${this.loc === "/PvPRules" ? "active" : ""}`}>
+                    <Link class="dropdown-item" to="/pvp-rules">PvP Rules</Link>
+                    </li>
+                    <li class={`nav-item  ${this.loc === "/CashHuntRules" ? "active" : ""}`}>
+                    <Link class="dropdown-item" to="/cash-hunt-rules">Cash Hunt Rules</Link>
+                    </li>
+                    <li class={`nav-item  ${this.loc === "/ModGuide" ? "active" : ""}`}>
+                    <Link class="dropdown-item" to="/mod-guide">Mod Guide</Link>
+                    </li>
+                    <NavDropdown.Divider />
+                    <li class={`nav-item  ${this.loc === "/PlayerSafetyPlan" ? "active" : ""}`}>
+                    <Link class="dropdown-item" to="/player-safety-plan">Player Safety Plan</Link>
+                    </li>
+                  </NavDropdown>
+    
+                  <li
+                    class={`nav-item  ${
+                      this.loc === "/Updates" ? "active" : ""
+                    }`}
+                  >
+                    <Link class="nav-link" to="/updates">
+                      Updates
+                    </Link>
+                  </li>
+    
+                  <li
+                    class={`nav-item  ${
+                      this.loc === "/Leaderboard" ? "active" : ""
+                    }`}
+                  >
+                    <Link class="nav-link" to="/leaderboard">
+                      Leaderboard
+                    </Link>
+                  </li>
+    
+                  <li
+                    class={`nav-item  ${
+                      this.loc === "/contact" ? "active" : ""
+                    }`}
+                  >
+                    <Link class="nav-link" to="/contact">
+                      Contact
+                    </Link>
+                  </li>
+    
+                  <li
+                    class={`nav-item  ${
+                      this.loc === "/LogIn" ? "active" : ""
+                    }`}
+                  >
+                    <Link class="nav-link" to="/login">
+                      Log In
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+        </div>
+      );
+    }
+
+  }
+
 }
 
 export default withRouter(Navigation);
