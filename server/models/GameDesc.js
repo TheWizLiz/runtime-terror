9943e88445fe07b3826a606d76f6a0e5add73172
Kingston <liamyoung@ufl.edu>
2021-04-10 21:17:16 -0400

    Removed a comment. See previous commit for major changes

--- server/models/GameDesc.js
+++ server/models/GameDesc.js
@@ -4,7 +4,6 @@ const GameDescSchema = new mongoose.Schema({
   game_id: { type: String, required: true },
   location: { type: String, default: 'UF Campus' },
   description: { type: String, default: '' },
-  // photo: { type: Buffer, contentType: String },
   registration_start: { type: Date, default: Date.now() },
   registration_deadline: { type: Date, required: true }
 })
