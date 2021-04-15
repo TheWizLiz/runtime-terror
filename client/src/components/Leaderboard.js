792d6197fd073a80256eeee6e26864256b672901
Kingston <liamyoung@ufl.edu>
2021-04-15 19:40:20 -0400

    Initial Leaderboard branch

--- client/src/components/Leaderboard.js
+++ client/src/components/Leaderboard.js
@@ -1,15 +1,17 @@
 import React from "react";
+import LeaderboardTable from '../components/leaderboard/LeaderboardTable.js'
 
-function Leaderboard() {
+function Leaderboard () {
   return (
     <div className="leaderboard">
       <div class="container">
         <div class="row align-items-center my-5">
           <h1 class="font-weight-light">Leaderboard</h1>
+          <LeaderboardTable />
         </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default Leaderboard;
+export default Leaderboard
---
+++ client/src/components/leaderboard/LeaderboardTable.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+class LeaderboardTable extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {}
+  }
+
+  render () {
+    return (
+      <div className='container'>
+        <div className='row'>
+          <p>Table</p>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default LeaderboardTable
