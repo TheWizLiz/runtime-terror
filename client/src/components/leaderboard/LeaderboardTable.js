2b3f80ada85ffb43e6bae584dd472fb0578a64db
Kingston <liamyoung@ufl.edu>
2021-04-15 19:55:30 -0400

    table header

--- client/src/components/leaderboard/LeaderboardTable.js
+++ client/src/components/leaderboard/LeaderboardTable.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Table from 'react-bootstrap/Table'
 
 class LeaderboardTable extends Component {
   constructor (props) {
@@ -10,7 +11,16 @@ class LeaderboardTable extends Component {
     return (
       <div className='container'>
         <div className='row'>
-          <p>Table</p>
+          <Table id='leaderboard' variant='dark'>
+            <thead>
+              <th>#</th>
+              <th>Player</th>
+              <th>Kills</th>
+              <th>Deaths</th>
+              <th>Team</th>
+              <th>Remaining Lives</th>
+            </thead>
+          </Table>
         </div>
       </div>
     )
