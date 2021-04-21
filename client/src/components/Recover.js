82bed1d64d35ed3302371eb882cc2d65070ec3af
TheWizLiz <gabriel.turmail@gmail.com>
2021-04-21 18:31:04 -0400

    Fixed Formatting

--- client/src/components/Recover.js
+++ client/src/components/Recover.js
@@ -12,7 +12,7 @@ function Recover () {
     return (
       <div className="forgot">
         <div class="container">
-          <div class="row align-items-center my-5">
+          <div class="row align-items-center">
             <div class="col">
               <h1 class="font-weight-light">Reset Password</h1><br />
               <p>Please enter a new password.</p>
