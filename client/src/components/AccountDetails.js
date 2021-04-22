243141a088a446092c1af64c109a2f658fe4d0dc
Kingston <liamyoung@ufl.edu>
2021-04-21 20:01:21 -0400

    Added QR code to Account Details page

--- client/src/components/AccountDetails.js
+++ client/src/components/AccountDetails.js
@@ -1,12 +1,14 @@
 import React from "react";
 import AccountDisplay from './account/AccountDisplay'
+import PlayerQRCode from './PlayerQRCode'
 
 function AccountDetails() {
   return (
     <div className="accountDetails">
       <div class="container">
-        <div class="row align-items-center">
+        <div class="align-items-center my-5">
           <h1 class="font-weight-light">Account Details</h1>
+          <PlayerQRCode />
         </div>
         <div class="mt-3">
         <AccountDisplay />
